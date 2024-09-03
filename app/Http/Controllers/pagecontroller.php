e7b216ac48160030642a66c256b267c27cdc1acc
Admin <snmvaliente@gmail.com>
2024-09-03 23:59:49 +0800

    Added Debugbar

--- app/Http/Controllers/pagecontroller.php
+++ app/Http/Controllers/pagecontroller.php
@@ -2,74 +2,84 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use Nette\Utils\Image;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 use App\Http\Controllers\Controller;
-use Intervention\Image\ImageManager;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rule;
 use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
+use Barryvdh\Debugbar\Facades\Debugbar;
 
 class pagecontroller extends Controller
 {
-    public function showcorrecthomepage() {
+    public function showcorrecthomepage()
+    {
         if (auth()->check()) {
             return view('homepage-feed');
         } else {
             return view('home-guest');
         }
     }
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
         $incomingfields = $request->validate([
-            'loginusername'=>'required',
-            'loginpassword'=>'required'
+            'loginusername' => 'required',
+            'loginpassword' => 'required',
         ]);
 
-        if (auth()->attempt(['username'=>$incomingfields['loginusername'], 'password'=>$incomingfields['loginpassword']])) {
+        if (auth()->attempt(['username' => $incomingfields['loginusername'], 'password' => $incomingfields['loginpassword']])) {
             $request->session()->regenerate();
-            return redirect('/')->with('success','You have successfully logged in!');
+            return redirect('/')->with('success', 'You have successfully logged in!');
         } else {
             return redirect('/')->with('failure', 'invalid login');
         }
     }
 
-    public function logout() {
+    public function logout()
+    {
         auth()->logout();
-        return redirect('/')->with('failure','You have successfully logged out!');
+        return redirect('/')->with('failure', 'You have successfully logged out!');
     }
 
-    public function register(Request $request) {
-        
+    public function register(Request $request)
+    {
+
         $incomingfields = $request->validate([
-            'username'=>['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
-            'email'=>['required', 'email', Rule::unique('users', 'email')],
-            'password'=>['required', 'min:8', 'confirmed']
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'confirmed'],
         ]);
-        
+
         $incomingfields['password'] = bcrypt($incomingfields['password']);
 
         User::create($incomingfields);
 
         return 'Hey! welcome to my vlog controller page!';
 
-   }
+    }
 
-   public function profile(User $user) {
+    public function profile(User $user)
+    {
         return view('profile-posts', ['avatar' => $user->avatar, 'username' => $user->username, 'posts' => $user->posts()->latest()->get(), 'postCount' => $user->posts()->count()]);
-   }
+    }
 
-   public function showAvatarForm() {
+    public function showAvatarForm()
+    {
+        Debugbar::info('AVATAR');
+        Debugbar::error('AVATAR');
+        Debugbar::warning('AVATAR');
         return view('avatar-form');
-   }
+    }
 
-   public function storeAvatar(Request $request) {
+    public function storeAvatar(Request $request)
+    {
         $request->validate([
-            'avatar'=>'required|image|max:4000'
+            'avatar' => 'required|image|max:4000',
         ]);
 
         $user = auth()->user();
-        
+
         $filename = $user->id . '-' . uniqid() . '.jpg';
 
         $manager = new ImageManager(new Driver());
@@ -87,7 +97,7 @@ class pagecontroller extends Controller
         }
 
         return back()->with('success', 'Updated!');
-   }
+    }
 
 //    public function storeAvatar(Request $request) {
 //         $request->file('avatar')->store('public/avatar');
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\pagecontroller;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use Barryvdh\Debugbar\Facades\Debugbar;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,16 +15,17 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
 Route::get('/', function () {
+    Debugbar::info('sad');
     return view('home-guest');
 });
 
-Route::get('/admins-only', function() {
+Route::get('/admins-only', function () {
+    Debugbar::info('sad');
     return 'Admin Ka!';
-})->middleware('can:visitAdminPage, post'); 
-
+})->middleware('can:visitAdminPage, post');
 
 // User Routes
 Route::get('/', [pagecontroller::class, "showcorrecthomepage"])->name('login');
@@ -33,9 +36,16 @@ Route::get('/manage-avatar', [pagecontroller::class, "showAvatarForm"])->middlew
 Route::post('/manage-avatar', [pagecontroller::class, "storeAvatar"])->middleware('MustBeLoggedIn');
 
 // Posting Routes
-Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('MustBeLoggedIn');
-Route::post('/create-post', [PostController::class, 'storeData'])->middleware('MustBeLoggedIn');
-Route::get('/post/{post}', [PostController::class, 'viewSinglePost'])->middleware('MustBeLoggedIn');
+// Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('MustBeLoggedIn');
+// Route::post('/create-post', [PostController::class, 'storeData'])->middleware('MustBeLoggedIn');
+// Route::get('/post/{post}', [PostController::class, 'viewSinglePost'])->middleware('MustBeLoggedIn');
+
+Route::middleware('MustBeLoggedIn')->group(function () {
+    Debugbar::info('Info!');
+    Route::get('/create-post', [PostController::class, 'showCreateForm']);
+    Route::post('/create-post', [PostController::class, 'storeData']);
+    Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
+});
 
 Route::delete('/post/{post}', [PostController::class, 'delete'])->middleware('can:delete,post');
 Route::get('/post/{post}/edit', [PostController::class, 'showEditForm'])->middleware('can:update,post');
